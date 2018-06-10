07f41bf158ce5d13beb3bf3ed4b991425814970e
k <id6982808@gmail.com>
2018-06-10 17:49:48 +0900

    ソース追加

---
+++ Chapter03001/src/Chapter03032.java
@@ -0,0 +1,10 @@
+// 数値の切り上げ、切り捨て、四捨五入を行う
+public class Chapter03032 {
+	public static void main(String[] args) {
+		double a = 5.43d;
+
+		System.out.println(Math.ceil(a));
+		System.out.println(Math.floor(a));
+		System.out.println(Math.round(a));
+	}
+}
---
+++ Chapter03001/src/Chapter03033.java
@@ -0,0 +1,9 @@
+// 平方根、立方根を求める
+public class Chapter03033 {
+	public static void main(String[] args) {
+		double d = 5.6;
+
+		System.out.println(Math.sqrt(d));
+		System.out.println(Math.cbrt(d));
+	}
+}
